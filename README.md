107db9bd8ffb1b39adab1ffa3e22519987976567
George Svarovsky <gsvarovsky@gmail.com>
2018-04-29 11:13:51 +0100

    Final polishing

--- README.md
+++ README.md
@@ -7,8 +7,12 @@ Console-based adventure game built by the Coding Club!
 1. Press **F12** or right-click anywhere and select "Inspect"
 1. Select the "Console" tab
 1. You will be told what you can do. Each possible instruction is a _function_ that you can type into the Console.
-   * You must also add a pair of brackets, like this: `goNorth()`
+   * You must also include a pair of brackets, for example like this: `sayHi()`
+   * Sometimes you need to include a _parameter_, like this: `say("Hi")`. Pay close attention to the instructions to see whether the parameter needs to be in quotes.
    * then, press **Enter**
+
+_Hint:_ If you mess up or die, use the Back button in the browser to try again.
+_Hint:_ Javascript is _case sensitive_. You have to use the correct lowercase and uppercase letters.
 
 ## Technical
 `imageOptim -a -d .`
--- player.js
+++ player.js
@@ -1,29 +1,30 @@
-var loc = window.location.href, qMarkIndex = loc.indexOf('?');
-
-have = qMarkIndex > -1 && qMarkIndex < loc.length - 1 ?
-  loc.substring(qMarkIndex + 1).split('&').map(decodeURIComponent) : [];
+function locId(loc) {
+  if (loc) {
+    var url = document.createElement('a');
+    url.href = loc;
+    var path = url.pathname.split('/').map(decodeURIComponent);
+    return path.slice(-2).join('/');
+  } else {
+    return locId(window.location.href);
+  }
+}
 
 function go(loc) {
   if (loc) {
-    window.location.href = loc + '?' + have.map(encodeURIComponent).join('&');
+    var id = locId(loc);
+    if (id !== 'george/home.html' && been.indexOf(id) > -1) {
+      console.log("You have already been there.");
+    } else {
+      window.location.href = loc + '?'
+        + 'have=' + have.map(encodeURIComponent).join(',')
+        + '&been=' + been.map(encodeURIComponent).concat(locId(window.location.href)).join(',');
+    }
   } else {
     console.log("Sorry, you can't go that way.");
   }
 }
 
-north = east = west = south = null;
-// Pick up the page's directions on the next tick
-setTimeout(function () {
-  var directions = { north: north, east: east, west: west, south: south };
-  var available = Object.keys(directions).filter(dir => directions[dir]);
-  if (available.length) {
-    console.log('You can go(' + available.join('/') + ')');
-  }
-}, 0);
-
-here = []; // To be overridden in the page
-
-pickUp = function () {
+function pickUp() {
   for (var i = 0; i < arguments.length; i++) {
     var what = arguments[i];
     var inHere = here.indexOf(what);
@@ -34,10 +35,10 @@ pickUp = function () {
       console.log('What ' + what + '?');
     }
   }
-  console.log('You have: ' + have);
+  logHave();
 };
 
-drop = function () {
+function drop() {
   for (var i = 0; i < arguments.length; i++) {
     var what = arguments[i];
     var inHave = have.indexOf(what);
@@ -48,8 +49,39 @@ drop = function () {
       console.log('What ' + what + '?');
     }
   }
-  console.log('You have: ' + have);
+  logHave();
 };
 
-console.log('You have: ' + have);
-console.log('You can pickUp("x") or drop("x")');
+function logHave() {
+  console.log(have.length ? 'You have: ' + have : 'You have nothing with you');
+}
+
+(function (loc) {
+  var qMarkIndex = loc.indexOf('?');
+  var params = (qMarkIndex > -1 && qMarkIndex < loc.length - 1 ?
+    loc.substring(qMarkIndex + 1).split('&') : []).reduce(function (params, param) {
+      var eq = param.split('='), lhs = eq[0], rhs = eq[1];
+      if (lhs && rhs) {
+        params[decodeURIComponent(lhs)] = rhs.split(',').map(decodeURIComponent);
+      }
+      return params;
+    }, {});
+
+  have = params.have || [];
+  been = params.been || [];
+
+  north = east = west = south = null;
+  // Pick up the page's directions on the next tick
+  setTimeout(function () {
+    var directions = { north: north, east: east, west: west, south: south };
+    var available = Object.keys(directions).filter(dir => directions[dir]);
+    if (available.length) {
+      console.log('You can go(' + available.join('/') + ')');
+    }
+  }, 0);
+
+  here = []; // To be overridden in the page
+
+  logHave();
+  console.log('You can pickUp("something") or drop("something")');
+})(window.location.href);
